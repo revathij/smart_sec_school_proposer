098737a4667bdc2788de86c12e835622f6a50331
wbadmin <>
2019-02-27 20:41:47 +0800

    {/src/main/java/com/myspace/smart_sec_school_proposer/Applicant.java}

--- src/main/java/com/myspace/smart_sec_school_proposer/Applicant.java
+++ src/main/java/com/myspace/smart_sec_school_proposer/Applicant.java
@@ -6,12 +6,97 @@ package com.myspace.smart_sec_school_proposer;
 
 public class Applicant implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Applicant() {
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "Postal Code")
+	private java.lang.Integer postal_Code;
+	@org.kie.api.definition.type.Label(value = "Mode of transport")
+	private java.util.List<java.lang.String> transport_Mode;
+	@org.kie.api.definition.type.Label(value = "Expected or Prelims Score")
+	private java.lang.Integer t_score;
+	@org.kie.api.definition.type.Label(value = "Mother Tongue ")
+	private java.util.List<java.lang.String> motherTongue;
+	@org.kie.api.definition.type.Label(value = "Resident Status")
+	private java.util.List<java.lang.String> residence_Status;
+	@org.kie.api.definition.type.Label(value = "Gender")
+	private java.util.List<java.lang.String> gender;
 
+	public Applicant() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Integer getPostal_Code() {
+		return this.postal_Code;
+	}
+
+	public void setPostal_Code(java.lang.Integer postal_Code) {
+		this.postal_Code = postal_Code;
+	}
+
+	public java.util.List<java.lang.String> getTransport_Mode() {
+		return this.transport_Mode;
+	}
+
+	public void setTransport_Mode(
+			java.util.List<java.lang.String> transport_Mode) {
+		this.transport_Mode = transport_Mode;
+	}
+
+	public java.lang.Integer getT_score() {
+		return this.t_score;
+	}
+
+	public void setT_score(java.lang.Integer t_score) {
+		this.t_score = t_score;
+	}
+
+	public java.util.List<java.lang.String> getMotherTongue() {
+		return this.motherTongue;
+	}
+
+	public void setMotherTongue(java.util.List<java.lang.String> motherTongue) {
+		this.motherTongue = motherTongue;
+	}
+
+	public java.util.List<java.lang.String> getResidence_Status() {
+		return this.residence_Status;
+	}
+
+	public void setResidence_Status(
+			java.util.List<java.lang.String> residence_Status) {
+		this.residence_Status = residence_Status;
+	}
+
+	public java.util.List<java.lang.String> getGender() {
+		return this.gender;
+	}
+
+	public void setGender(java.util.List<java.lang.String> gender) {
+		this.gender = gender;
+	}
+
+	public Applicant(java.lang.String name, java.lang.Integer postal_Code,
+			java.util.List<java.lang.String> transport_Mode,
+			java.lang.Integer t_score,
+			java.util.List<java.lang.String> motherTongue,
+			java.util.List<java.lang.String> residence_Status,
+			java.util.List<java.lang.String> gender) {
+		this.name = name;
+		this.postal_Code = postal_Code;
+		this.transport_Mode = transport_Mode;
+		this.t_score = t_score;
+		this.motherTongue = motherTongue;
+		this.residence_Status = residence_Status;
+		this.gender = gender;
+	}
 
 }
