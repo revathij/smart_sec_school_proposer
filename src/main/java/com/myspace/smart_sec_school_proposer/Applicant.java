895c644bd23e07ddb67a9d8151d0baa1861eb566
wbadmin <>
2019-03-01 11:21:05 +0800

    {/src/main/java/com/myspace/smart_sec_school_proposer/Applicant.java}

--- src/main/java/com/myspace/smart_sec_school_proposer/Applicant.java
+++ src/main/java/com/myspace/smart_sec_school_proposer/Applicant.java
@@ -8,20 +8,20 @@ public class Applicant implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@org.kie.api.definition.type.Label(value = "Name")
+	@org.kie.api.definition.type.Label("Name")
 	private java.lang.String name;
-	@org.kie.api.definition.type.Label(value = "Postal Code")
+	@org.kie.api.definition.type.Label("Postal Code")
 	private java.lang.Integer postal_Code;
-	@org.kie.api.definition.type.Label(value = "Mode of transport")
-	private java.util.List<java.lang.String> transport_Mode;
-	@org.kie.api.definition.type.Label(value = "Expected or Prelims Score")
+	@org.kie.api.definition.type.Label("Mode of transport")
+	private String transport_Mode;
+	@org.kie.api.definition.type.Label("Expected or Prelims Score")
 	private java.lang.Integer t_score;
-	@org.kie.api.definition.type.Label(value = "Mother Tongue ")
-	private java.util.List<java.lang.String> motherTongue;
-	@org.kie.api.definition.type.Label(value = "Resident Status")
-	private java.util.List<java.lang.String> residence_Status;
-	@org.kie.api.definition.type.Label(value = "Gender")
-	private java.util.List<java.lang.String> gender;
+	@org.kie.api.definition.type.Label("Mother Tongue ")
+	private String motherTongue;
+	@org.kie.api.definition.type.Label("Resident Status")
+	private String residence_Status;
+	@org.kie.api.definition.type.Label("Gender")
+	private String gender;
 
 	public Applicant() {
 	}
@@ -42,15 +42,6 @@ public class Applicant implements java.io.Serializable {
 		this.postal_Code = postal_Code;
 	}
 
-	public java.util.List<java.lang.String> getTransport_Mode() {
-		return this.transport_Mode;
-	}
-
-	public void setTransport_Mode(
-			java.util.List<java.lang.String> transport_Mode) {
-		this.transport_Mode = transport_Mode;
-	}
-
 	public java.lang.Integer getT_score() {
 		return this.t_score;
 	}
@@ -59,37 +50,42 @@ public class Applicant implements java.io.Serializable {
 		this.t_score = t_score;
 	}
 
-	public java.util.List<java.lang.String> getMotherTongue() {
+	public java.lang.String getTransport_Mode() {
+		return this.transport_Mode;
+	}
+
+	public void setTransport_Mode(java.lang.String transport_Mode) {
+		this.transport_Mode = transport_Mode;
+	}
+
+	public java.lang.String getMotherTongue() {
 		return this.motherTongue;
 	}
 
-	public void setMotherTongue(java.util.List<java.lang.String> motherTongue) {
+	public void setMotherTongue(java.lang.String motherTongue) {
 		this.motherTongue = motherTongue;
 	}
 
-	public java.util.List<java.lang.String> getResidence_Status() {
+	public java.lang.String getResidence_Status() {
 		return this.residence_Status;
 	}
 
-	public void setResidence_Status(
-			java.util.List<java.lang.String> residence_Status) {
+	public void setResidence_Status(java.lang.String residence_Status) {
 		this.residence_Status = residence_Status;
 	}
 
-	public java.util.List<java.lang.String> getGender() {
+	public java.lang.String getGender() {
 		return this.gender;
 	}
 
-	public void setGender(java.util.List<java.lang.String> gender) {
+	public void setGender(java.lang.String gender) {
 		this.gender = gender;
 	}
 
 	public Applicant(java.lang.String name, java.lang.Integer postal_Code,
-			java.util.List<java.lang.String> transport_Mode,
-			java.lang.Integer t_score,
-			java.util.List<java.lang.String> motherTongue,
-			java.util.List<java.lang.String> residence_Status,
-			java.util.List<java.lang.String> gender) {
+			java.lang.String transport_Mode, java.lang.Integer t_score,
+			java.lang.String motherTongue, java.lang.String residence_Status,
+			java.lang.String gender) {
 		this.name = name;
 		this.postal_Code = postal_Code;
 		this.transport_Mode = transport_Mode;
